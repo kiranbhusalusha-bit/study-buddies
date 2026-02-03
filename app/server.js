f7efbf852cd67b8797e58996c48c73dfd603d19e
ushakiranbhusal <kiranbhusalusha@gmail.com>
2026-02-03 23:07:01 +0000

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Study Buddies is running!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
 
